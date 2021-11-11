8ae1459fccd93d76d26ea2008ce7505372dc9961
Mikhail Tsarev <78015319+Mikhail-Tsarev@users.noreply.github.com>
2021-11-11 13:19:52 +0300

    task03 finished

--- homework1/task03.py
+++ homework1/task03.py
@@ -12,4 +12,10 @@ from typing import Tuple
 
 
 def find_maximum_and_minimum(file_name: str) -> Tuple[int, int]:
-    pass
+    lst = []
+    with open(file_name) as fi:
+        for line in fi:
+            if line == "\n":
+                continue
+            lst.append(int(line.strip()))
+    return min(lst), max(lst)
--- tests/homework1/test_task03.py
+++ tests/homework1/test_task03.py
@@ -3,6 +3,16 @@ import pytest
 from homework1.task03 import find_maximum_and_minimum
 
 
-def test_empty_seq_case():
-    """Testing that empty sequence returns False"""
-    assert not find_maximum_and_minimum([])
+def test_normal_file_case():
+    """Testing file with line-delimited integers"""
+    assert find_maximum_and_minimum("task03_test_file1.txt") == (1, 10)
+
+
+def test__file_with_empty_strings_case():
+    """Testing file with line-delimited integers and empty strings"""
+    assert find_maximum_and_minimum("task03_test_file2.txt") == (-4, 20)
+
+
+def test__file_with_one_int_case():
+    """Testing file with only one integer"""
+    assert find_maximum_and_minimum("task03_test_file3.txt") == (1, 1)
