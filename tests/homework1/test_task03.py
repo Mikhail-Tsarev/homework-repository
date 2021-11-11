b5d6f073573be0f26a3e14f9ab971242dc4ccf39
Mikhail Tsarev <78015319+Mikhail-Tsarev@users.noreply.github.com>
2021-11-11 15:35:39 +0300

    path for test files changed

--- tests/homework1/test_task03.py
+++ tests/homework1/test_task03.py
@@ -3,14 +3,20 @@ from homework1.task03 import find_maximum_and_minimum
 
 def test_normal_file_case():
     """Testing file with line-delimited integers"""
-    assert find_maximum_and_minimum("task03_test_file1.txt") == (1, 10)
+    assert find_maximum_and_minimum(
+        "./tests/homework1/task03_test_file1.txt"
+    ) == (1, 10)
 
 
-def test__file_with_empty_strings_case():
+def test_file_with_empty_strings_case():
     """Testing file with line-delimited integers and empty strings"""
-    assert find_maximum_and_minimum("task03_test_file2.txt") == (-4, 20)
+    assert find_maximum_and_minimum(
+        "./tests/homework1/task03_test_file2.txt"
+    ) == (-4, 20)
 
 
-def test__file_with_one_int_case():
+def test_file_with_one_int_case():
     """Testing file with only one integer"""
-    assert find_maximum_and_minimum("task03_test_file3.txt") == (1, 1)
+    assert find_maximum_and_minimum(
+        "./tests/homework1/task03_test_file3.txt"
+    ) == (1, 1)
