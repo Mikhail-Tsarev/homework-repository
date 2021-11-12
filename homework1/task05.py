3a8ba3858d3b5de68ee83fa46aba4aa84efb7f1b
Mikhail Tsarev <78015319+Mikhail-Tsarev@users.noreply.github.com>
2021-11-12 07:23:06 +0300

    fixed code style issues

--- homework1/task05.py
+++ homework1/task05.py
@@ -10,7 +10,7 @@ from typing import List
 
 
 def find_maximal_subarray_sum(nums: List[int], k: int) -> int:
-    """Function finds a sub-array with length less equal to "k", with max sum"""
+    """Function finds a sub-array with length less equal to k, with max sum"""
 
     if len(nums) == 0:
         return "Invalid input: empty list"
