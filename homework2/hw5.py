34ec521a0464f3b61e9e57ad95ff96a9b6d77b8b
Mikhail Tsarev <78015319+Mikhail-Tsarev@users.noreply.github.com>
2021-11-15 15:57:26 +0300

    fixed code style issues

--- homework2/hw5.py
+++ homework2/hw5.py
@@ -4,9 +4,12 @@ positional and keyword arguments.
 Write a function that accept any iterable of unique values and then
 it behaves as range function:
 import string
-assert = custom_range(string.ascii_lowercase, 'g') == ['a', 'b', 'c', 'd', 'e', 'f']
-assert = custom_range(string.ascii_lowercase, 'g', 'p') == ['g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o']
-assert = custom_range(string.ascii_lowercase, 'p', 'g', -2) == ['p', 'n', 'l', 'j', 'h']
+assert = custom_range(string.ascii_lowercase, 'g')
+== ['a', 'b', 'c', 'd', 'e', 'f']
+assert = custom_range(string.ascii_lowercase, 'g', 'p')
+== ['g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o']
+assert = custom_range(string.ascii_lowercase, 'p', 'g', -2)
+== ['p', 'n', 'l', 'j', 'h']
 """
 
 import string
