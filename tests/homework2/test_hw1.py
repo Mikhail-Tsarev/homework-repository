e598547c4dd952ade5b6c1de6b7c5d545c56ae4c
Mikhail Tsarev <78015319+Mikhail-Tsarev@users.noreply.github.com>
2021-11-16 10:40:01 +0300

    fixed code style issues

--- tests/homework2/test_hw1.py
+++ tests/homework2/test_hw1.py
@@ -1,12 +1,7 @@
-from homework2.hw1 import (
-    count_non_ascii_chars,
-    count_punctuation_chars,
-    get_longest_diverse_words,
-    get_most_common_non_ascii_char,
-    get_rarest_char,
-)
+import homework2.hw1 as hw1
 
 file_name = "./homework2/data.txt"
+# file_name = "D:\YandexDisk\Python\homework-repository\homework2\data.txt"
 
 
 def test_func_1():
@@ -24,32 +19,32 @@ def test_func_1():
         "werkstättenlandschaft",
         "wiederbelebungsübungen",
     ]
-    assert get_longest_diverse_words(file_name) == result
+    assert hw1.get_longest_diverse_words(file_name) == result
 
 
 def test_func_2():
     """Testing that function 2 returns
     right answer"""
     result = "î"
-    assert get_rarest_char(file_name) == result
+    assert hw1.get_rarest_char(file_name) == result
 
 
 def test_func_3():
     """Testing that function 3 returns
     right answer"""
     result = 5475
-    assert count_punctuation_chars(file_name) == result
+    assert hw1.count_punctuation_chars(file_name) == result
 
 
 def test_func_4():
     """Testing that function 4 returns
     right answer"""
     result = 2972
-    assert count_non_ascii_chars(file_name) == result
+    assert hw1.count_non_ascii_chars(file_name) == result
 
 
 def test_func_5():
     """Testing that function 4 returns
     right answer"""
     result = "ä"
-    assert get_most_common_non_ascii_char(file_name) == result
+    assert hw1.get_most_common_non_ascii_char(file_name) == result
