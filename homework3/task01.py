4a26f0aa3d3e4ab7f75ceb1210ed267cb487a3ac
Mikhail Tsarev <78015319+Mikhail-Tsarev@users.noreply.github.com>
2021-11-18 16:40:33 +0300

    task01 and test_task01 finished

--- homework3/task01.py
+++ homework3/task01.py
@@ -3,19 +3,27 @@ from typing import Callable
 
 
 def cashe_times(times: int = None) -> Callable:
+    """
+    Parametrized decoratoк for cashe function
+    Returns cashe 'times' times only
+
+    :param times: Number of times the cashe is returned
+    :return: Cashe func
+    """
+
     def cache(func: Callable) -> Callable:
         cache_data = {}
-        t = times + 1
+        if times is not None:
+            t = times + 1
 
         @wraps(func)
         def wrapper(*args):
             nonlocal t
             if times is not None:
                 t -= 1
-                if t == 0:
+                if t < 0:
                     del cache_data[args]
                     t = times
-
             if args not in cache_data:
                 cache_data[args] = func(*args)
             return cache_data[args]
@@ -23,13 +31,3 @@ def cashe_times(times: int = None) -> Callable:
         return wrapper
 
     return cache
-
-
-if __name__ == "__main__":
-
-    @cashe_times(3)
-    def f():
-        return input("? ")
-
-    for i in range(10):
-        print(f())
