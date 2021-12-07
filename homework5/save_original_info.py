44118a5ea647ec9c46046592f4a4d445b1dc7a8e
Mikhail Tsarev <78015319+Mikhail-Tsarev@users.noreply.github.com>
2021-12-07 15:36:00 +0200

    Homework5 (#5)
    * task 2 finished
    
    * fixed codestyle issues
    
    * homework 5 finished
    
    * fixed error in test_oop_1
    
    * fixed error in test_oop_1
    
    * fixed error in test_oop_1
    
    * doc strings added

---
+++ homework5/save_original_info.py
@@ -0,0 +1,27 @@
+from typing import Callable
+
+
+def save_info(original_func: Callable) -> Callable:
+    """Decorator which saves original
+    function doc and name attributes
+
+    :param original_func: Function for saving its info"""
+
+    def wraps(wrapping_func: Callable) -> Callable:
+        wrapping_func.__doc__ = original_func.__doc__
+        wrapping_func.__name__ = original_func.__name__
+        wrapping_func.__original_func = original_func
+        return wrapping_func
+
+    return wraps
+
+
+def print_result(func):
+    @save_info(func)
+    def wrapper(*args, **kwargs):
+        """Function-wrapper which print result of an original function"""
+        result = func(*args, **kwargs)
+        print(result)
+        return result
+
+    return wrapper
