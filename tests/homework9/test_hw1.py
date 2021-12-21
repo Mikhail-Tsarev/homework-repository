d22e2d2fdaccda0bb8faee5cbeb15d63c4c9b5d4
Mikhail Tsarev <78015319+Mikhail-Tsarev@users.noreply.github.com>
2021-12-21 16:46:12 +0300

    fixed code style issues one more time

--- tests/homework9/test_hw1.py
+++ tests/homework9/test_hw1.py
@@ -1,7 +1,7 @@
 from homework9.hw1 import merge_sorted_files
 
 file1 = "./homework9/file1.txt"
-file2 = "./homework9/file1.txt"
+file2 = "./homework9/file2.txt"
 # file1 = "D:\YandexDisk\Python\homework-repository\homework9\\file1.txt"
 # file2 = "D:\YandexDisk\Python\homework-repository\homework9\\file2.txt"
 
