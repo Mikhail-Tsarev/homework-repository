f7e7ba816cc2cee396c67e85bf98a1e93516d3d0
Mikhail Tsarev <78015319+Mikhail-Tsarev@users.noreply.github.com>
2021-12-21 20:45:32 +0300

    fixed codestyle issues

--- tests/homework9/test_hw3.py
+++ tests/homework9/test_hw3.py
@@ -6,15 +6,15 @@ from homework9.hw3 import universal_file_counter
 def test__case_without_tokenizer(tmpdir):
     """Testing case without tokenizer"""
 
-    tmpdir.join(f"text1.txt").write("0\n1\n2\n3\n4\n5\n6\n")
-    tmpdir.join(f"text2.txt").write("7\n8\n9\n")
-    tmpdir.join(f"text3.txt").write("10\n11\n12\n")
+    tmpdir.join("text1.txt").write("0\n1\n2\n3\n4\n5\n6\n")
+    tmpdir.join("text2.txt").write("7\n8\n9\n")
+    tmpdir.join("text3.txt").write("10\n11\n12\n")
     assert universal_file_counter(Path(str(tmpdir)), "txt") == 13
 
 
 def test_case_with_split_tokenizer(tmpdir):
     """Testing case with tokenizer"""
 
-    tmpdir.join(f"text1.txt").write("0 1\n2\n3\n")
-    tmpdir.join(f"text2.txt").write("4\n5\n6 7 8 9\n")
+    tmpdir.join("text1.txt").write("0 1\n2\n3\n")
+    tmpdir.join("text2.txt").write("4\n5\n6 7 8 9\n")
     assert (universal_file_counter(Path(str(tmpdir)), "txt", str.split)) == 10
