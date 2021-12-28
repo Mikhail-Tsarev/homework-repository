51dbafa36c18041316c83e6e9a5382ffb6d19984
Mikhail Tsarev <78015319+Mikhail-Tsarev@users.noreply.github.com>
2021-12-28 22:20:23 +0300

    hw10 done but with crutches

--- homework10/stonks.py
+++ homework10/stonks.py
@@ -1,21 +1,27 @@
 import json
-import operator
 import os
 import re
+import time
+from functools import lru_cache
+from multiprocessing.pool import ThreadPool as Pool
 
 import requests
 from bs4 import BeautifulSoup
+
+all_companies_data = []
 
 
 def get_main_page_link(url, domain_zone):
     return url.split(domain_zone)[0] + domain_zone
 
 
+@lru_cache
 def get_html(url):
-    r = requests.get(url)
+    r = requests.get(url, timeout=None)
     return r.text
 
 
+@lru_cache
 def get_pages_list(main_url):
     domain = get_main_page_link(main_url, ".com")
 
@@ -25,6 +31,7 @@ def get_pages_list(main_url):
     return pages_list
 
 
+@lru_cache
 def get_all_companies_links(main_url):
     domain = get_main_page_link(main_url, ".com")
     pages_list = get_pages_list(main_url)
@@ -42,17 +49,15 @@ def get_all_companies_links(main_url):
     return all_companies_links
 
 
-def usd_to_rub(price_in_usd):
+@lru_cache
+def get_usd_rate():
     link = "https://www.cbr.ru/scripts/XML_daily.asp"
     soup = BeautifulSoup(get_html(link), "lxml-xml")
-    usd_rate = float(
-        soup.find("Valute", ID="R01235").Value.text.replace(",", ".")
-    )
 
-    return round(usd_rate * price_in_usd, 2)
+    return float(soup.find("Valute", ID="R01235").Value.text.replace(",", "."))
 
 
-def get_company_data(company_page):
+def get_company_data(company_page, main_url, usd_rate):
     company_info = {}
 
     soup = BeautifulSoup(get_html(company_page), "lxml")
@@ -70,10 +75,13 @@ def get_company_data(company_page):
         .text.strip()
     )
 
-    price_in_usd = soup.find(
-        "span", class_="price-section__current-value"
-    ).text
-    price = usd_to_rub(float(price_in_usd))
+    price_in_usd = float(
+        soup.find("span", class_="price-section__current-value").text.replace(
+            ",", ""
+        )
+    )
+
+    price = round(usd_rate * price_in_usd, 2)
 
     try:
         pe = float(
@@ -82,15 +90,16 @@ def get_company_data(company_page):
             .split("\r")[0]
         )
     except:
-        pe = None
+        pe = float("inf")
 
     try:
         low_52 = (
             soup.find("div", text=re.compile("52 Week Low"))
             .parent.text.strip()
             .split("\r")[0]
+            .replace(",", "")
         )
-    except:
+    except AttributeError:
         low_52 = None
 
     try:
@@ -98,29 +107,59 @@ def get_company_data(company_page):
             soup.find("div", text=re.compile("52 Week High"))
             .parent.text.strip()
             .split("\r")[0]
+            .replace(",", "")
         )
-    except:
+    except AttributeError:
         high_52 = None
 
-    potential_profit = None
+    potential_profit = "-100000000%"
     if high_52 and low_52:
         potential_profit = (
             f"{(float(high_52) - float(low_52)) / float(low_52) :.2%}"
         )
 
+    pages_list = get_pages_list(main_url)
+
+    growth = "-100000000%"
+    for page in pages_list:
+        p = get_html(page)
+        if name not in p:
+            continue
+        else:
+            soup = BeautifulSoup(p, "lxml")
+            # hrf = "/stocks/" + code.lower() + "-stock"
+            growth = (
+                soup.find("a", title=name)
+                .parent.parent.select("span:last-child")[-1]
+                .text
+            )
+            break
+
     company_info["name"] = name
     company_info["code"] = code
     company_info["price"] = price
     company_info["p/e"] = pe
+    company_info["growth"] = growth
     company_info["potential profit"] = potential_profit
+
+    # print(name, growth)
 
     return company_info
 
 
 def top_ten(data, sort_key, reverse=True):
-    return sorted(data, key=operator.itemgetter(sort_key), reverse=reverse)[
-        :10
-    ]
+    if isinstance(data[0][sort_key], str):
+        return sorted(
+            data,
+            key=lambda d: float(d[sort_key][:-1]) / 100,
+            reverse=reverse,
+        )[:10]
+
+    return sorted(
+        data,
+        key=lambda d: d[sort_key],
+        reverse=reverse,
+    )[:10]
 
 
 def dump_to_json(data, filename):
@@ -129,32 +168,48 @@ def dump_to_json(data, filename):
         json.dump(data, f_out)
 
 
+def append_info(company_page, main_url, usd):
+    global all_companies_data
+    # random_number = random.uniform(0.01, 0.5)
+    # time.sleep(random_number)
+
+    all_companies_data.append(get_company_data(company_page, main_url, usd))
+
+
 def main():
     main_url = "https://markets.businessinsider.com/index/components/s&p_500"
-    # all_companies_data = []
-    #
-    # for company_page in get_all_companies_links(main_url)[:15]:
-    #     all_companies_data.append(get_company_data(company_page))
-    #
-    # top_10_by_price = top_ten(all_companies_data, "price")
-    # top_10_by_low_pe = top_ten(all_companies_data, "p/e", False)
-    # top_10_p_profit = top_ten(all_companies_data, "potential profit")
-    #
-    # print(top_10_by_price)
-    # print(top_10_by_low_pe)
-    # print(top_10_p_profit)
-    # dump_to_json(top_10_by_price, "top_10_by_price")
-    # dump_to_json(top_10_by_low_pe, "top_10_by_low_pe")
-    # dump_to_json(top_10_p_profit, "top_10_p_profit")
+    usd = get_usd_rate()
 
-    soup = BeautifulSoup(get_html(main_url), "lxml")
-    name = "3M"
-    growth = (
-        (soup.find("a", title=name))
-        .parent.parent.select("span:last-child")[-1]
-        .text
-    )
-    print(growth)
+    pool_size = 8
+    pool = Pool(pool_size)
+
+    for company_page in get_all_companies_links(main_url):
+        pool.apply_async(append_info, (company_page, main_url, usd))
+    pool.close()
+    pool.join()
+    time.sleep(4)
+
+    print(f"Processed {len(all_companies_data)} companies")
+
+    top_10_by_price = top_ten(all_companies_data, "price")
+    top_10_by_low_pe = top_ten(all_companies_data, "p/e", False)
+    top_10_by_growth = top_ten(all_companies_data, "growth")
+    top_10_p_profit = top_ten(all_companies_data, "potential profit")
+
+    # print(*top_10_by_price, sep="\n")
+    # print()
+    # print(*top_10_by_low_pe, sep="\n")
+    # print()
+    # print(*top_10_by_growth, sep="\n")
+    # print()
+    # print(*top_10_p_profit, sep="\n")
+
+    dump_to_json(top_10_by_price, "top_10_by_price")
+    dump_to_json(top_10_by_low_pe, "top_10_by_low_pe")
+    dump_to_json(top_10_by_growth, "top_10_by_growth")
+    dump_to_json(top_10_p_profit, "top_10_p_profit")
+
+    print("4 JSON files has been recorded")
 
 
 if __name__ == "__main__":
